89507e1c3f7fed3abb9f396ddcc0b6565c4bbcb9
Chix88 <38764019+Chix88@users.noreply.github.com>
2019-04-25 08:02:21 +0200

    Add files via upload

---
+++ bettingGame/bettingGame/Betters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bettingGame
+{
+    public class Betters
+    {
+        public int Money;
+        public int Bet;
+        public int BettingChoice;
+
+        public void Win()
+        {
+            this.Money =  (this.Money - this.Bet) + (this.Bet * 2);
+
+        }
+
+        public void Loose()
+        {
+            Money = Money - Bet;
+        }
+    }
+}
---
+++ bettingGame/bettingGame/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bettingGame
+{
+    public partial class Form1 : Form
+    {
+        private Betters _mike = new Betters();
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            RemplirForm1();
+
+        }
+        ImageRuning[] cats = new ImageRuning[3];
+
+        private void ToggleRace(int index, PictureBox catBox)
+        {
+            if (cats[index] == null)
+            {
+                cats[index]= new ImageRuning();
+                cats[index].NewPicture = catBox;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ToggleRace(0, pictureBox1);
+            
+            ToggleRace(1, pictureBox2);
+            
+            ToggleRace(2, pictureBox3);
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (cats[i] != null)
+                {
+                    cats[i].Move();
+                    if (cats[i].racing == false)
+                    {
+                        timer1.Stop();
+                    }
+                }
+                
+            }
+        }
+
+
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox1.CheckState == CheckState.Checked)
+            {
+                panel1.Enabled = true;
+            }
+            else
+            {
+                panel1.Enabled = false;
+            }
+        }
+
+        public void RemplirForm1()
+        {
+            
+           
+            
+        }
+
+        
+      
+        
+    }
+}
